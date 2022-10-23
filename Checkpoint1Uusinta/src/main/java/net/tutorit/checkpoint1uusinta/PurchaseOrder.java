582f91dea86a91083fe9922ea252378974478350
Susanna <mspitko@gmail.com>
2022-10-23 15:59:03 +0300

    uusinta

--- Checkpoint1Uusinta/src/main/java/net/tutorit/checkpoint1uusinta/PurchaseOrder.java
+++ Checkpoint1Uusinta/src/main/java/net/tutorit/checkpoint1uusinta/PurchaseOrder.java
@@ -5,7 +5,6 @@
 package net.tutorit.checkpoint1uusinta;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
