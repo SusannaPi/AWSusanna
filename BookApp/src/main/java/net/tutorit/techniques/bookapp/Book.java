8407a162f27bdfe524ad2e256b5f85e4600b65b8
Susanna <mspitko@gmail.com>
2022-10-23 08:28:54 +0300

    kokeilu

---
+++ BookApp/src/main/java/net/tutorit/techniques/bookapp/Book.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.tutorit.techniques.bookapp;
+
+/**
+ *
+ * @author Susanna
+ */
+public class Book {
+    String title;
+    String author;
+    double price;
+    int numCopies;
+    int yearPublished;
+    
+    
+    
+    
+}
